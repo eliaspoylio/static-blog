718cbdbfb527f3e32bcc6ddd679615dd0d7c7c72
Elias Pöyliö <elias.poylio@gmail.com>
2021-02-08 09:40:31 +0200

    style: code cleanup

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core'
-import { ScullyRoutesService } from '@scullyio/ng-lib'
-import { map } from 'rxjs/operators'
+import { Component } from '@angular/core';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog',
